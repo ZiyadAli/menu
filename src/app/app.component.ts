204aa55d9c936a617c82e364c3cf7ad195ef7007
Ziyad Ali <zey.990@gmail.com>
2024-06-09 00:55:13 +0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+  total:number = 0;
+  qty:number = 0;
+  constructor() {
+
+  }
+  foodmenu = [
+    { id: 1, name: "دجاج", price: 30, description: "دجاج مشوي على الفحم", img: "/assets/images/cheken1.jpg" },
+    { id: 2, name: "دجاج", price: 30, description: "دجاج شواية ", img: "/assets/images/cheken.jpg" },
+    { id: 3, name: "ايدام", price: 20, description: "ايدام خضار مشكل", img: "/assets/images/edam1.jpg" },
+    { id: 4, name: "ايدام", price: 20, description: "ايدام  بالدجاج على الفرن", img: "/assets/images/edam.jpg" }
+  ]
+
+  sum(event: any, price: any) {
+    const isChecked: boolean = event.target.checked;
+    if(isChecked){
+      this.total +=price
+      this.qty +=1
+    }else{
+      this.total -=price
+      this.qty -=1
+    }
+    console.log(isChecked);
+    console.log(price);
+  }
+
 }
